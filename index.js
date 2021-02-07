983759332dbbdccb18e62e6c3dd5ffdaa8370341
Pablo Sanchez <pablo.sanchez@aranova.es>
2021-02-07 15:05:06 +0100

    update for posstcss 8

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const postcss = require("postcss");
 
-module.exports = postcss.plugin("postcss-class-rename", options => {
+module.exports = (opts = {}) => {
   const isObject = typeof options === "object" && options instanceof Object;
 
   if (!isObject) {
     return;
   }
-
-  return root => {
-    const replaceClass = (pattern, replacement) => {
-      if (typeof pattern !== "string") {
-        return;
-      }
-
-      root.walk(rule => {
-        const regex = new RegExp(pattern, "gi");
-
-        if (rule.type === "rule") {
-          rule.selector = rule.selector.replace(regex, replacement);
-        } else if (rule.type === "atrule") {
-          rule.params = rule.params.replace(regex, replacement);
-        }
-      });
-    };
-
-    // Replace all defined pattern and replacement
-    Object.keys(options).forEach(function(p) {
-      replaceClass(p, options[p]);
-    });
-  };
-});
+  
+  function replaceClass(rule, pattern, replacement) {
+    if (typeof pattern !== "string") {
+      return;
+    }
+    const regex = new RegExp(pattern, "gi");
+    if (rule.type === "rule") {
+      rule.selector = rule.selector.replace(regex, replacement);
+    } else if (rule.type === "atrule") {
+      rule.params = rule.params.replace(regex, replacement);
+    }
+  }
+  
+ return {
+   postcssPlugin: 'postcss-class-rename',
+   AtRule (rule) {
+     Object.keys(options).forEach(p => {
+       replaceClass(rule, p, options[p]);
+     });
+   }
+ }
+}
+module.exports.postcss = true;
